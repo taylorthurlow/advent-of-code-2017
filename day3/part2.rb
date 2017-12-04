0849ec1a0015a5bb7f6620eae87d014496dcb131
Taylor Thurlow <taylorthurlow8@gmail.com>
2017-12-03 19:23:21 -0800

    Add day 3 part 2 solution

--- day3/part2.rb
+++ day3/part2.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 
-def spiral_matrix(input)
+def matrix_next_highest(input)
   # determine the size of the matrix by finding the next highest perfect square root
   matrix_size = Math.sqrt(input).ceil
   matrix_size += 1 if matrix_size % 2 == 0            # make sure the matrix size is odd
@@ -11,9 +11,7 @@ def spiral_matrix(input)
   steps = [:right, :up, :left, :down]                 # set up traversal order using directions
   step_num = 0
   row = col = (matrix_size - 1) / 2                   # calculate the start index
-  input_col = input_row = nil                         # vars for the final coordinates of our input value
   matrix[row][col] = 1                                # base case, we always start with 1 in the center
-  counter = 2
 
   # loop through values that represent the number of "moves" as we populate the matrix. we always
   # move i times, TWICE. For the very last iteration, we move i times, THRICE, finishing the matrix.
@@ -27,37 +25,27 @@ def spiral_matrix(input)
         col -= 1 if steps[step_num] == :left
         row += 1 if steps[step_num] == :down
 
-        matrix[row][col] = counter                    # set the value in the matrix
-        if counter == input                           # set the location of our input value if we found it
-          input_col = col
-          input_row = row
+        values = []
+        (-1..1).each do |j|                           # iterate through all adjacent values, being sure that
+          (-1..1).each do |k|                         # we are checking the bounds of our matrix
+            access_row = row + j
+            access_col = col + k
+            if !(j == 0 && k == 0) && access_row >= 0 && access_row < matrix_size && access_col >= 0 && access_col < matrix_size
+              values << matrix[row + j][col + k]
+            end
+          end
         end
-        counter += 1                                  # increment the value to be placed in the next spot
+
+        sum = values.sum
+        matrix[row][col] = sum                        # set the value in the matrix to the sum of the values
+        return sum if sum > input                     # we found the answer, return and forget the rest
       end
 
       step_num = (step_num + 1) % steps.count         # to go the next movement step, essentially looping
                                                       # circularly around the direction array
     end
   end
-
-  if [input_col, input_row].any?(&:nil?)              # catch base case of a 1x1 matrix
-    input_col = input_row = 0
-  end
-
-  return {
-    matrix: matrix,
-    input_col: input_col,
-    input_row: input_row
-  }
-end
-
-def manhattan_distance(matrix, row, col)
-  start = (matrix.first.length - 1) / 2
-  d_col = (col - start).abs                           # calc the column distance
-  d_row = (row - start).abs                           # calc the row distance
-  return d_col + d_row                                # add the differences to get the manhattan distance
 end
 
 input = 347991
-result = spiral_matrix(input)
-p manhattan_distance(result[:matrix], result[:input_row], result[:input_col])
+p matrix_next_highest(input)
