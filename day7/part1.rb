e6811647c7074f26ebf0018bac7a8db473f0390e
Taylor Thurlow <taylorthurlow8@gmail.com>
2017-12-07 01:10:36 -0800

    Remove unnecessary day 7 part 1 stuff, use one liner

--- day7/part1.rb
+++ day7/part1.rb
@@ -1,23 +1,3 @@
 #!/usr/bin/ruby
 
-class Program
-  attr_accessor :name, :weight, :above
-  def initialize(name, weight, above)
-    @name = name
-    @weight = weight
-    @above = above
-  end
-end
-
-programs = []
-data = File.readlines('data.txt').map { |d| d.scan(%r{([a-z]+) \(([0-9]+)\)( -> ([a-z, ]+))?}).first }
-
-data.each do |d|
-  d.delete_at(2)
-  d[2] = d[2].split(', ') unless d[2].nil?
-  programs << Program.new(d[0], d[1].to_i, d[2])
-end
-
-# find the difference between the program list and the list of all child
-# programs, this is guaranteed to be our root program
-puts programs.map(&:name) - programs.map(&:above).flatten.compact.uniq
+p File.read('data.txt').scan(%r{([a-z]+)}).flatten.group_by{ |d| d}.keep_if{ |_,e| e.size==1 }.keys.first
