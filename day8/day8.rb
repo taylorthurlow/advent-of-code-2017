4b0956230e7ff60b45f59498e957791ac5f6283f
Taylor Thurlow <taylorthurlow8@gmail.com>
2018-11-30 22:56:41 -0800

    Day 8 solution cleanup

--- day8/day8.rb
+++ day8/day8.rb
@@ -1,13 +1,12 @@
 #!/usr/bin/env ruby
 
 data = File.read('data.txt').chomp.split("\n")
-r = /(\w+) (\w+) ([\d+-]+) if (\w+) ([<>=!]+) ([\d+-]+)/
 
 # Part 1
 regs = Hash.new 0
 
 data.each do |instr|
-  m = instr.match r
+  m = instr.match /(\w+) (\w+) ([\d+-]+) if (\w+) ([<>=!]+) ([\d+-]+)/
   next unless eval("#{regs[m[4].to_sym]} #{m[5]} #{m[6]}")
   eval("regs[:#{m[1]}] #{m[2] == 'inc' ? '+=' : '-='} #{m[3]}")
 end
@@ -16,10 +15,10 @@ puts regs.values.max
 
 # Part 2
 regs = Hash.new 0
-highest = -9999999999
+highest = -9999
 
 data.each do |instr|
-  m = instr.match r
+  m = instr.match /(\w+) (\w+) ([\d+-]+) if (\w+) ([<>=!]+) ([\d+-]+)/
   next unless eval("#{regs[m[4].to_sym]} #{m[5]} #{m[6]}")
   eval("regs[:#{m[1]}] #{m[2] == 'inc' ? '+=' : '-='} #{m[3]}")
   highest = [highest, regs.values.max].max
